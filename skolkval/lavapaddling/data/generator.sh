11fb1f0aa37225ce284b7a6e5377d5521b125ce6
Matistjati <matistjati@outlook.com>
2023-02-19 01:02:03 +0100

    [lavapaddling] generator fixes

--- skolkval/lavapaddling/data/generator.sh
+++ skolkval/lavapaddling/data/generator.sh
@@ -13,19 +13,15 @@ sample 2
 sample 3
 
 group g1 20
-limits max_n 1000
+limits max_k=1 maxh=100
 tc_manual g1
 
-group g2 20
-limits max_n 1000
+group g2 40
+include_group g1
+limits max_k=1
 tc_manual g2
 
-group g3 20
-limits max_n 1000000
+group g3 40
+include_group g2
+include_group samplegroup
 tc_manual g3
-
-group g4 20
-tc_manual g4
-
-group g5 20
-tc_manual g5
